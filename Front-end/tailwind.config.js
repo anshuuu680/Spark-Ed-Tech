f300c5deb92e4874ac20799ebf169931d4ca8ccd
Anshu patidar <ap214893@gmail.com>
2025-04-03 13:18:17 +0530

    first commit

--- Front-end/tailwind.config.js
+++ Front-end/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default  {
   darkMode: 'class', // Enables dark mode with 'class'
   content: [
     './pages/**/*.{js,jsx}',
