365d1f52866b5747c458b2ecfe010df9fa305750
anshuuu680 <ap214893@gmail.com>
2025-06-17 18:06:13 +0530

    fix

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -1,16 +1,16 @@
-import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Header from "./components/Header/Header.jsx";
 import Dashboard from "./components/Pages/dashboard/Dashboard.jsx";
 import Task from "./components/Pages/Tasks/Task.jsx";
 import Feed from "./components/Pages/Feed/Feed.jsx";
 import FeedPage from "./components/Pages/Feed/FeedPage.jsx";
-import UserPageLayout from "./components/layout/UserPageLayout";
-import UserPage from "./components/Pages/Feed/UserPage";
-import UserPosts from "./components/Pages/Feed/UserPosts";
-import UserQuestions from "./components/Pages/Feed/UserQuestions";
+import UserPageLayout from "./components/layout/UserPageLayout.jsx";
+import UserPage from "./components/Pages/Feed/UserPage.jsx";
+import UserPosts from "./components/Pages/Feed/UserPosts.jsx";
+import UserQuestions from "./components/Pages/Feed/UserQuestions.jsx";
 import Login from "./components/auth/Login.jsx";
 import SignUp from "./components/auth/SignUp.jsx";
-import ForgotPassword from "./components/auth/ForgotPassword";
+import ForgotPassword from "./components/auth/ForgotPassword.jsx";
 import HomePage from "./components/Pages/Home/HomePage.jsx";
 import AdminNavbar from "./components/Admin/Navbar/AdminNavbar.jsx";
 import AdminDashboard from "./components/Admin/Dashboard/AdminDashboard.jsx";
@@ -18,30 +18,29 @@ import AdminCourses from "./components/Admin/Course/AdminCourses.jsx";
 import AdminNotification from "./components/Admin/Notification/AdminNotification.jsx";
 import AdminTransactions from "./components/Admin/Transactions/AdminTransactions.jsx";
 import Chat from "./components/Pages/Chat/Chat.jsx";
-import DynamicChat from './components/Pages/Chat/DynamicChat';
-import Layout from "./components/layout/Layout";
-import ProfileLayout from "./components/layout/ProfileLayout";
-import MyProfile from "./components/Pages/Profile/MyProfile";
-import AdminLayout from './components/Admin/Layout/AdminLayout';
-import CourseDetails from './components/Admin/Course/CourseDetails';
-import PasswordChanged from './components/auth/PasswordChanged';
-import VerifyOtp from './components/auth/VerifyOtp';
-import Courses from './components/Pages/Course/Courses.jsx'
-import CourseDetail from "./components/Pages/Course/CourseDetail";
-import HomeLayout from './components/layout/HomeLayout.jsx';
-import ILyout from './components/Instructor/ILyout.jsx';
-import IDashboard from './components/Instructor/IDashboard.jsx';
-import ICourses from './components/Instructor/ICourses.jsx';
-import ICreate from './components/Instructor/ICreate.jsx';
-import IPayment from './components/Instructor/IPayment.jsx';
-import ISection from './components/Instructor/ISection.jsx';
-import ISignUp from './components/Instructor/ISignUp.jsx';
-import IVerify from './components/Instructor/IVerify.jsx';
-import ILogin from './components/Instructor/ILogin.jsx';
-import ICDetails from './components/Instructor/ICDetails.jsx';
-import MyClassroom from './components/Pages/Course/MyClassroom.jsx';
-import MyCourses from './components/Pages/Course/MyCourses.jsx';
-
+import DynamicChat from "./components/Pages/Chat/DynamicChat.jsx";
+import Layout from "./components/layout/Layout.jsx";
+import ProfileLayout from "./components/layout/ProfileLayout.jsx";
+import MyProfile from "./components/Pages/Profile/MyProfile.jsx";
+import AdminLayout from "./components/Admin/Layout/AdminLayout.jsx";
+import CourseDetails from "./components/Admin/Course/CourseDetails.jsx";
+import PasswordChanged from "./components/auth/PasswordChanged.jsx";
+import VerifyOtp from "./components/auth/VerifyOtp.jsx";
+import Courses from "./components/Pages/Course/Courses.jsx";
+import CourseDetail from "./components/Pages/Course/CourseDetail.jsx";
+import HomeLayout from "./components/layout/HomeLayout.jsx";
+import ILyout from "./components/Instructor/ILyout.jsx";
+import IDashboard from "./components/Instructor/IDashboard.jsx";
+import ICourses from "./components/Instructor/ICourses.jsx";
+import ICreate from "./components/Instructor/ICreate.jsx";
+import IPayment from "./components/Instructor/IPayment.jsx";
+import ISection from "./components/Instructor/ISection.jsx";
+import ISignUp from "./components/Instructor/ISignUp.jsx";
+import IVerify from "./components/Instructor/IVerify.jsx";
+import ILogin from "./components/Instructor/ILogin.jsx";
+import ICDetails from "./components/Instructor/ICDetails.jsx";
+import MyClassroom from "./components/Pages/Course/MyClassroom.jsx";
+import MyCourses from "./components/Pages/Course/MyCourses.jsx";
 
 export {
   Navbar,
@@ -76,7 +75,7 @@ export {
   CourseDetail,
   HomeLayout,
   ILyout,
-  IDashboard, 
+  IDashboard,
   ICourses,
   ICreate,
   IPayment,
@@ -86,6 +85,5 @@ export {
   ILogin,
   ICDetails,
   MyClassroom,
-  MyCourses
-  
+  MyCourses,
 };
